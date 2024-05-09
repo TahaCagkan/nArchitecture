2eca4dcbb5636b4d9a38745145bbebf71cc5103e
Taha Cantürk <tahacagkan@outlook.com>
2024-05-09 23:51:16 +0300

    Add project files.

---
+++ Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    //Application ile ilgili tüm registration işlemleri yapabilirim.
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+            return services;
+        }
+    }
+}
---
+++ WebApi/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class BaseController:ControllerBase
+    {
+        //Eğer daha önce Mediator injection yapılmışsa onu döndür yoksa null ise .RequestServices.GetService<IMediator>(); YAP
+        private IMediator? _mediator;
+        protected IMediator? Mediator => _mediator??=HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
